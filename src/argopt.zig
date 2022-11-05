c174bd5966b7b8c66f82061cd196076371eb7f8a
jiacai2050 <dev@liujiacai.net>
2022-11-05 23:28:20 +0800

    parse struct option pass

--- src/argopt.zig
+++ src/argopt.zig
@@ -7,12 +7,12 @@ const CliOption = struct {
     version: bool = false,
     exclude: ?[]const u8 = null,
 
-    const shorthands = .{
+    const __shorts__ = .{
         .help = .h,
         .version = .v,
     };
 
-    const messages = .{
+    const _messages_ = .{
         .help = "show help",
     };
 
@@ -22,39 +22,48 @@ const CliOption = struct {
     };
 };
 
-const ParseError = error{
-    NoProgram,
-};
+const ParseError = error{ NoProgram, NoLongOption };
+const OptionError = ParseError || std.mem.Allocator.Error;
+
 const OptionParser = struct {
     program: []const u8 = undefined,
     parsedOptions: OptionFields,
     allocator: std.mem.Allocator,
 
+    const Self = @This();
+
     const OptionType = enum(u32) {
+        const REQUIRED_VERSION_SHIFT = 16;
+
         Int,
         Bool,
         Float,
         String,
-        RequiredInt = REQUIRED_VERSION_SHIFT,
+        RequiredInt = @This().REQUIRED_VERSION_SHIFT,
         RequiredBool,
         RequiredFloat,
         RequiredString,
 
-        const REQUIRED_VERSION_SHIFT = 16;
-
-        fn from_zig_type(T: type, is_option: bool) !OptionType {
-            const base_type = switch (@typeInfo(T)) {
+        fn from_zig_type(comptime T: type, comptime is_option: bool) OptionType {
+            const base_type: @This() = switch (@typeInfo(T)) {
                 .Int, .Bool, .Float => .Int,
                 .Array => .String,
                 .Optional => |opt_info| return from_zig_type(opt_info.child, true),
+                .Pointer => |ptr_info|
+                // only support []const u8
+                if (ptr_info.size == .Slice and ptr_info.child == u8 and ptr_info.is_const)
+                    .String
+                else {
+                    @compileError("not supported option type:" ++ @typeName(T));
+                },
                 else => {
-                    std.log.err("not support type:{s}", .{@typeName(T)});
-                    return error.InvalidType;
+                    @compileError("not supported option type:" ++ @typeName(T));
                 },
             };
-            return @intToEnum(@This(), base_type + if (is_option) REQUIRED_VERSION_SHIFT else 0);
+            return @intToEnum(@This(), @enumToInt(base_type) + if (is_option) @This().REQUIRED_VERSION_SHIFT else 0);
         }
     };
+
     const OptionField = struct {
         long_name: []const u8,
         short_name: ?u8 = null,
@@ -62,26 +71,51 @@ const OptionParser = struct {
         opt_type: OptionType,
         default_value: ?*const anyopaque,
     };
+
     const OptionFields = std.StringHashMap(OptionField);
 
-    const Self = @This();
-
-    pub fn init(allocator: std.mem.Allocator, comptime T: type) !Self {
+    // `T` is a struct, which define options
+    pub fn init(allocator: std.mem.Allocator, comptime T: type) anyerror!Self {
         const option_type_info = @typeInfo(T);
         if (option_type_info != .Struct) {
-            @compileError("option should be struct, found " ++ @typeName(T));
+            @compileError("option should be defined using struct, found " ++ @typeName(T));
         }
 
         var opts = OptionFields.init(allocator);
         inline for (option_type_info.Struct.fields) |fld| {
             const long_name = fld.name;
-            try opts.put(long_name, .{
+            const opt_type = OptionType.from_zig_type(fld.field_type, false);
+            try opts.put(long_name, OptionField{
                 .long_name = long_name,
                 .opt_type = opt_type,
                 .default_value = fld.default_value,
             });
         }
+
+        // parse short names
+        if (@hasDecl(T, "__shorts__")) {
+            const short_type_info = @typeInfo(@TypeOf(T.__shorts__));
+            if (short_type_info != .Struct) {
+                @compileError("short option should be defined using struct, found " ++ @typeName(T));
+            }
+
+            inline for (short_type_info.Struct.fields) |fld| {
+                const long_name = fld.name;
+                var option = opts.getPtr(long_name) orelse {
+                    std.log.err("no such long option, value: {s}", .{long_name});
+                    return error.NoLongOtion;
+                };
+
+                const short_name = @field(T.__shorts__, long_name);
+                if (@typeInfo(@TypeOf(short_name)) != .EnumLiteral) {
+                    @compileError("short option value must be literal enum, found " ++ @typeName(T));
+                }
+                option.short_name = @tagName(short_name)[0];
+            }
+        }
+
         return .{
+            .program = "test",
             .allocator = allocator,
             .parsedOptions = OptionFields.init(allocator),
         };
@@ -99,11 +133,16 @@ const OptionParser = struct {
 };
 
 pub const log_level: std.log.Level = .info;
-test "parse cli" {
-    const allocator = std.testing.allocator;
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
     const parser = try OptionParser.init(allocator, CliOption);
 
-    _ = parser;
+    std.log.warn("parser is {any}", .{parser});
+    // _ = parser;
     // const opt = CliOption{};
     // const shorts = @TypeOf(CliOption.shorthands);
     // inline for (std.meta.fields(shorts)) |field| {
