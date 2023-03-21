9e55a7a9f6e7e0781b0f64738f550fb8762528e6
jiacai2050 <dev@liujiacai.net>
2023-03-21 23:27:29 +0800

    chore: refactor build.zig

--- build.zig
+++ build.zig
@@ -1,40 +1,31 @@
 const std = @import("std");
+const Build = std.Build;
+const FileSource = Build.FileSource;
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
-
-    const simargs = b.dependency("simargs", .{
-        .target = target,
-        .optimize = optimize,
-    });
+    const simargs_dep = b.dependency("simargs", .{});
 
     inline for (.{
         .{
-            buildTree(b, optimize, target, simargs),
+            buildTree(b, optimize, target, simargs_dep),
             "tree",
-            "src/tree.zig",
         },
         .{
-            buildLoc(b, optimize, target, simargs),
+            buildLoc(b, optimize, target, simargs_dep),
             "loc",
-            "src/loc.zig",
         },
         .{
-            buildYes(
-                b,
-                optimize,
-                target,
-            ),
+            buildYes(b, optimize, target),
             "yes",
-            "src/yes.zig",
         },
     }) |prog| {
-        buildRunTestStep(b, prog.@"0", prog.@"1", prog.@"2");
+        buildRunTestStep(b, prog.@"0", prog.@"1");
     }
 }
 
-fn buildRunTestStep(b: *std.build.Builder, exe: *std.build.CompileStep, comptime name: []const u8, root_file: []const u8) void {
+fn buildRunTestStep(b: *std.build.Builder, exe: *std.build.CompileStep, comptime name: []const u8) void {
     const run_cmd = exe.run();
     if (b.args) |args| {
         run_cmd.addArgs(args);
@@ -44,38 +35,35 @@ fn buildRunTestStep(b: *std.build.Builder, exe: *std.build.CompileStep, comptime
     run_step.dependOn(&run_cmd.step);
 
     const exe_tests = b.addTest(.{
-        .root_source_file = .{ .path = root_file },
+        .root_source_file = .{ .path = "src/" ++ name ++ ".zig" },
     });
     const test_step = b.step("test-" ++ name, "Run " ++ name ++ " tests");
     test_step.dependOn(&exe_tests.step);
 }
 
-fn buildTree(b: *std.build.Builder, optimize: std.builtin.Mode, target: std.zig.CrossTarget, simargs: *std.build.Dependency) *std.build.CompileStep {
+fn buildTree(b: *std.build.Builder, optimize: std.builtin.Mode, target: std.zig.CrossTarget, simargs_dep: *std.build.Dependency) *std.build.CompileStep {
     const exe = b.addExecutable(.{
         .name = "tree",
-        .root_source_file = .{ .path = "src/tree.zig" },
+        .root_source_file = FileSource.relative("src/tree.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.addModule("simargs", simargs.module("simargs"));
+    exe.addModule("simargs", simargs_dep.module("simargs"));
     exe.install();
 
     return exe;
 }
 
-fn buildLoc(b: *std.build.Builder, optimize: std.builtin.Mode, target: std.zig.CrossTarget, simargs: *std.build.Dependency) *std.build.CompileStep {
+fn buildLoc(b: *std.build.Builder, optimize: std.builtin.Mode, target: std.zig.CrossTarget, simargs_dep: *std.build.Dependency) *std.build.CompileStep {
     const exe = b.addExecutable(.{
         .name = "loc",
-        .root_source_file = .{ .path = "src/loc.zig" },
+        .root_source_file = FileSource.relative("src/loc.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.addModule("simargs", simargs.module("simargs"));
-    const dep_table = b.dependency("table-helper", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.addModule("table-helper", dep_table.module("table-helper"));
+    exe.addModule("simargs", simargs_dep.module("simargs"));
+    const table_dep = b.dependency("table-helper", .{});
+    exe.addModule("table-helper", table_dep.module("table-helper"));
     exe.install();
 
     return exe;
@@ -84,7 +72,7 @@ fn buildLoc(b: *std.build.Builder, optimize: std.builtin.Mode, target: std.zig.C
 fn buildYes(b: *std.build.Builder, optimize: std.builtin.Mode, target: std.zig.CrossTarget) *std.build.CompileStep {
     const exe = b.addExecutable(.{
         .name = "yes",
-        .root_source_file = .{ .path = "src/yes.zig" },
+        .root_source_file = FileSource.relative("src/yes.zig"),
         .target = target,
         .optimize = optimize,
     });
