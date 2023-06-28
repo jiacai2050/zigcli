5d17412f8096dc86622d844a01b71f58f1dc31e9
jiacai2050 <dev@liujiacai.net>
2023-06-28 21:41:39 +0800

    feat: add version

---
+++ src/common.zig
@@ -0,0 +1,5 @@
+const info = @import("build_info");
+
+pub fn print_build_info(writer: anytype) !void {
+    try writer.print("Build date: {d}\nGit commit: {s}", .{ info.build_date, info.git_commit });
+}
