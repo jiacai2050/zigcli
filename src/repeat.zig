e76bca7920966dadf283d2c5609991a23d85e896
jiacai2050 <dev@liujiacai.net>
2023-07-15 08:13:20 +0800

    refactor(repeat): add interval, remove stdout pipe, use inherit.

--- src/repeat.zig
+++ src/repeat.zig
@@ -1,5 +1,5 @@
 //! Repeat a command until it succeeds.
-//!
+
 const std = @import("std");
 const simargs = @import("simargs");
 const util = @import("util.zig");
@@ -14,19 +14,22 @@ pub fn main() !void {
 
     const opt = try simargs.parse(allocator, struct {
         max: ?usize,
+        interval: ?usize,
         version: bool = false,
         help: bool = false,
 
         pub const __shorts__ = .{
             .max = .m,
+            .interval = .i,
             .version = .v,
             .help = .h,
         };
 
         pub const __messages__ = .{
-            .help = "Print help information",
-            .version = "Print version",
             .max = "Max times to repeat",
+            .interval = "Pause interval(in seconds) between repeats",
+            .version = "Print version",
+            .help = "Print help information",
         };
     }, "command", util.get_build_info());
     defer opt.deinit();
@@ -51,38 +54,20 @@ pub fn main() !void {
                     keep_running = false;
                 }
             },
-            else => unreachable,
+            else => {},
+        }
+
+        if (keep_running) {
+            if (opt.args.interval) |pause| {
+                os.nanosleep(pause, 0);
+            }
         }
     }
 }
 
 fn run(allocator: mem.Allocator, argv: []const []const u8) !process.Child.Term {
     var child = process.Child.init(argv, allocator);
-    child.stdout_behavior = .Pipe;
-    child.stderr_behavior = .Pipe;
-    try child.spawn();
-
-    var poller = std.io.poll(allocator, enum { stdout, stderr }, .{
-        .stdout = child.stdout.?,
-        .stderr = child.stderr.?,
-    });
-    defer poller.deinit();
-
-    var out = std.io.getStdOut();
-    var err = std.io.getStdErr();
-    // Currently stdout & stderr will be mixed, no ordering guaranteed.
-    while (try poller.poll()) {
-        const child_stdout = poller.fifo(.stdout);
-        if (child_stdout.readableLength() > 0) {
-            try out.writeAll(child_stdout.readableSlice(0));
-            child_stdout.discard(child_stdout.count);
-        }
-        const child_stderr = poller.fifo(.stderr);
-        if (child_stderr.readableLength() > 0) {
-            try err.writeAll(child_stderr.readableSlice(0));
-            child_stderr.discard(child_stderr.count);
-        }
-    }
-
-    return try child.wait();
+    // By default, child will inherit stdout & stderr from its parents,
+    // so child's output will be redirect to output of parents.
+    return try child.spawnAndWait();
 }
